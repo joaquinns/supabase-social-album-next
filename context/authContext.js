5c1ceeedfd7b2f1130fe8033662bc99e5ecd6099
joaquinns <joaquindevv96@gmail.com>
2022-09-19 17:24:06 -0400

    created auth context

---
+++ context/authContext.js
@@ -0,0 +1,46 @@
+import { createContext, useContext, useEffect, useState } from 'react'
+import { supabase } from 'supabase'
+
+export const AuthContext = createContext({
+  user: null,
+  isLoading: true,
+  handleGoogleLogin: () => {}
+})
+
+export function AuthContextProvider({ children }) {
+  const [user, setUser] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const getSession = async () => {
+      const session = await supabase.auth.session()
+
+      setUser(session?.user ?? null)
+      setIsLoading(false)
+
+      await supabase.auth.onAuthStateChange(async (event, session) => {
+        setUser(session?.user ?? null)
+        setIsLoading(false)
+      })
+    }
+    getSession()
+  }, [])
+
+  const value = {
+    handleGoogleLogin: async () => {
+      setIsLoading(true)
+      await supabase.auth.signIn({
+        provider: 'google'
+      })
+      setIsLoading(false)
+    },
+    user,
+    isLoading
+  }
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+}
+
+export function useAuth() {
+  return useContext(AuthContext)
+}
