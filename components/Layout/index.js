5e03aec0b21a338d66df5722b49a18f72fc60418
joaquinns <joaquindevv96@gmail.com>
2022-09-28 18:21:31 -0400

    custom footer

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,12 +1,29 @@
 import { Header } from 'components/Header'
+import { AiFillGithub } from 'react-icons/ai'
 import PropTypes from 'prop-types'
 
 export const Layout = ({ children }) => {
   return (
     <div className='bg-zinc-900 text-white'>
       <Header />
-      <main className='container mx-auto min-h-screen pt-16'>{children}</main>
-      <footer>soy un footer</footer>
+      <main className='container mx-auto min-h-screen pt-16 pb-4'>
+        {children}
+      </main>
+      <footer
+        className='bg-black py-6
+      font-bold flex justify-center items-center gap-2
+      '
+      >
+        <a
+          href='https://github.com/joaquinns/'
+          target='_blank'
+          className='flex gap-2 items-center justify-center'
+          rel='noreferrer'
+        >
+          <AiFillGithub size='2rem' />
+          <span className='hover:underline'>check the source code</span>
+        </a>
+      </footer>
     </div>
   )
 }
