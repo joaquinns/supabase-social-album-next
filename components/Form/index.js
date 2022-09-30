93e100e3cef5dc2f8e512bfaf28572f8fe12013b
joaquinns <joaquindevv96@gmail.com>
2022-09-30 16:50:36 -0400

    added spinner, styles and router logic

--- components/Form/index.js
+++ components/Form/index.js
@@ -1,10 +1,14 @@
 import { useState } from 'react'
 import { useAuth } from 'context/authContext'
 import { createAlbum } from 'services/albums'
+import { Spinner } from 'components/Spinner'
+import { useRouter } from 'next/router'
+import { IoCreateSharp } from 'react-icons/io5'
 
 export const Form = () => {
   const { user } = useAuth()
   const [isLoading, setIsLoading] = useState(false)
+  const router = useRouter()
 
   const handleAlbumCreate = async (e) => {
     e.preventDefault()
@@ -19,31 +23,40 @@ export const Form = () => {
       userId
     })
     setIsLoading(false)
-    console.log('creado perro!', albumCreated)
     error && console.log(error)
+    console.log('creado perro!', albumCreated)
+    router.push('/')
   }
 
   return (
-    <form className='flex flex-col gap-4' onSubmit={handleAlbumCreate}>
+    <form
+      className='w-full md:w-3/4 lg:w-1/2 flex flex-col gap-4'
+      onSubmit={handleAlbumCreate}
+    >
       <input
         name='name'
         type='text'
-        placeholder='name of the album'
-        className='p-2 rounded bg-zinc-800'
+        placeholder='Name of the album'
+        className='p-2 md:p-3 rounded bg-zinc-800'
       />
       <textarea
         name='description'
-        className='p-2 rounded bg-zinc-800'
-        placeholder='description'
+        className='p-2 md:p-3 rounded bg-zinc-800 resize-none h-32'
+        placeholder='Description'
       ></textarea>
 
-      <button className='px-4 py-2 bg-zinc-700'>
-        {isLoading ? (
-          <div className='border-2 border-gray-500 border-t-teal-500 border-t-4 animate-spin rounded-full h-6 w-6 flex mx-auto justify-center'></div>
-        ) : (
-          'Create album :D'
-        )}
-      </button>
+      <div className='flex w-full justify-center'>
+        <button className='px-4 py-3 md:px-4 md:py-3 bg-zinc-700 font-semibold rounded hover:bg-zinc-800 ease-in-out duration-150'>
+          {isLoading ? (
+            <Spinner />
+          ) : (
+            <span className='flex gap-2 items-center justify-center'>
+              Create
+              <IoCreateSharp size='1.5rem' />
+            </span>
+          )}
+        </button>
+      </div>
     </form>
   )
 }
