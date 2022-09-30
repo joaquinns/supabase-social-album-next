a16d3fcea555e08912b7dd68cf19af285d2502fb
joaquinns <joaquindevv96@gmail.com>
2022-09-30 16:55:17 -0400

    added local photos provider and refactor styles

--- pages/album/[id].js
+++ pages/album/[id].js
@@ -1,56 +1,34 @@
-import { useState } from 'react'
-import Image from 'next/image'
 import { Layout } from 'components/Layout'
-import { getPhotosAlbum, getUserAlbum } from 'services/albums'
-import { Form } from 'components/Form'
+import { getUserAlbum } from 'services/albums'
+import { UploadButton } from 'components/UploadButton'
+// import { usePhotos } from 'hooks/usePhotos'
+import { ImageGrid } from 'components/ImagesGrid'
+import { PhotosProvider } from 'context/photosContext'
 
-export default function AlbumID({ album, photos }) {
-  const [showForm, setShowForm] = useState(false)
-
+export default function AlbumID({ album, albumId }) {
   return (
     <Layout>
-      <div className='flex 1 1 gap-4'>
-        {showForm && <Form uploadImageForm albumId={album.id} />}
-        <button
-          className='bg-zinc-700 p-4 font-bold text-xl rounded'
-          onClick={() => setShowForm(!showForm)}
-        >
-          Add photo
-        </button>
-      </div>
-      <h1>Este es el id del album: {album.id}</h1>
-      <h1>title: {album.name}</h1>
-      <p>{album.description}</p>
-      <div className='grid grid-cols-auto-grid gap-4'>
-        {photos
-          ? photos.map((photo) => (
-              <Image
-                key={photo.id}
-                src={photo.img_url}
-                width={150}
-                height={200}
-                className='rounded'
-                objectFit='cover'
-                alt='photo of the album'
-              />
-            ))
-          : 'Theres not images yet'}
-      </div>
+      <PhotosProvider albumId={albumId}>
+        <div className='flex flex-col md:flex-row justify-center items-center gap-3 mt-4'>
+          <UploadButton albumId={album.id} />
+        </div>
+        <h1 className='text-2xl font-bold'>{album.name}</h1>
+        <p className='text-lg mb-4'>{album.description}</p>
+        <ImageGrid albumId={albumId} />
+      </PhotosProvider>
     </Layout>
   )
 }
 
 export const getServerSideProps = async (ctx) => {
-  const albumId = await ctx.query.id
+  const albumId = await Number(ctx.query.id)
   const response = await getUserAlbum(albumId)
-  const photosResponse = await getPhotosAlbum(albumId)
-  const photos = photosResponse.data
   const album = response.data[0]
 
   return {
     props: {
       album,
-      photos
+      albumId
     }
   }
 }
