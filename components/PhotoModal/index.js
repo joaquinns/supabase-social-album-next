d7f6bd05aaac9e278f86ad3269e849e7f83722ad
joaquinns <joaquindevv96@gmail.com>
2022-09-30 16:53:25 -0400

    refactor styles

--- components/PhotoModal/index.js
+++ components/PhotoModal/index.js
@@ -2,9 +2,15 @@ import Image from 'next/image'
 import { Modal } from 'components/Modal'
 import { deletePhoto, updateAlbum } from 'services/albums'
 import { useModal } from 'hooks/useModal'
+import { useRouter } from 'next/router'
+import { usePhotosAlbum } from 'context/photosContext'
+import { AiFillDelete } from 'react-icons/ai'
+import { BsBookmarkStarFill } from 'react-icons/bs'
 
 export const PhotoModal = ({ photo }) => {
+  const { setPhotos } = usePhotosAlbum()
   const { handleCloseModal, handleModal, showModal } = useModal()
+  const router = useRouter()
 
   const handleMakeCover = async (albumId, imgURL) => {
     const [data, error] = await updateAlbum({
@@ -15,22 +21,29 @@ export const PhotoModal = ({ photo }) => {
       console.log(error)
     }
     console.log(data)
+    return router.push(`/`)
   }
 
-  const handleDeletePhoto = async (photoId) => {
+  const handleDeletePhoto = async (photoId, albumId) => {
     console.log(photoId, 'este es el que quieres borrar')
     const [data, error] = await deletePhoto(photoId)
     if (error) {
       return console.log({ error })
     }
+    setPhotos((prevPhotos) =>
+      prevPhotos.filter((photo) => photo.id !== photoId)
+    )
+
+    await updateAlbum({ albumId, photoCoverURL: null })
+    handleCloseModal()
     return console.log('BORRADO!', data)
   }
   return (
     <div className='relative h-[150px] w-full'>
       <Modal isOpen={showModal} closeModal={handleCloseModal}>
         {console.log(photo.id)}
-        <article className='flex items-center h-full flex-col gap-2 lg:flex-row'>
-          <div className='relative w-3/4 h-full'>
+        <article className='flex flex-1 lg:flex-none items-center h-full flex-col gap-2 lg:flex-row'>
+          <div className='relative w-full lg:w-3/4 h-full'>
             <Image
               src={photo.img_url}
               layout='fill'
@@ -39,20 +52,33 @@ export const PhotoModal = ({ photo }) => {
               alt='photo of the album'
             />
           </div>
-          <div className='w-1/3 h-full flex flex-col justify-start'>
-            <p>{photo.description}</p>
-            <button
-              onClick={() => handleMakeCover(photo.photo_album, photo.img_url)}
-              className='py-2 px-4 bg-zinc-700'
-            >
-              Make it cover
-            </button>
-            <button
-              onClick={() => handleDeletePhoto(photo.id)}
-              className='py-2 px-4 bg-zinc-700'
-            >
-              delete
-            </button>
+          <div className='w-full lg:w-1/3 h-1/3 lg:h-full flex flex-col justify-center lg:justify-start gap-2'>
+            <p className='h-full p-2 rounded font-semibold'>
+              {photo.description}
+            </p>
+
+            <div className='flex gap-1 justify-between items-center'>
+              <button
+                onClick={() =>
+                  handleMakeCover(photo.photo_album, photo.img_url)
+                }
+                className='py-2 px-3 md:px-4 text-sm md:text-base bg-zinc-900 hover:opacity-60 rounded font-bold flex gap-2 items-center justify-center'
+              >
+                Cover
+                <span>
+                  <BsBookmarkStarFill size='1.3rem' />
+                </span>
+              </button>
+              <button
+                onClick={() => handleDeletePhoto(photo.id, photo.photo_album)}
+                className='py-2 px-3 md:px-4 text-sm md:text-base bg-zinc-700 flex gap-2 items-center justify-center hover:bg-red-600 ease duration-200 rounded font-bold'
+              >
+                Delete
+                <span>
+                  <AiFillDelete size='1.3rem' />
+                </span>
+              </button>
+            </div>
           </div>
         </article>
       </Modal>
