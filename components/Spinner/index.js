16d83e603a509cd6eea3878e5af8bd4dd1969248
joaquinns <joaquindevv96@gmail.com>
2022-09-30 16:51:29 -0400

    created spinner component for buttons

---
+++ components/Spinner/index.js
@@ -0,0 +1,5 @@
+export const Spinner = () => {
+  return (
+    <div className='border-4 border-gray-500 border-t-teal-500 border-t-4 animate-spin rounded-full h-6 w-6 flex mx-auto justify-center'></div>
+  )
+}
