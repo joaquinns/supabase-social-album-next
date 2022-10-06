53b0ed4e7d02dae5e421a284af6cd24ad121d616
joaquinns <joaquindevv96@gmail.com>
2022-10-06 13:41:31 -0400

    added styles and head meta tag

--- pages/album/[id].js
+++ pages/album/[id].js
@@ -1,22 +1,36 @@
 import { Layout } from 'components/Layout'
+import Head from 'next/head'
 import { getUserAlbum } from 'services/albums'
 import { UploadButton } from 'components/UploadButton'
-// import { usePhotos } from 'hooks/usePhotos'
 import { ImageGrid } from 'components/ImagesGrid'
 import { PhotosProvider } from 'context/photosContext'
 
 export default function AlbumID({ album, albumId }) {
   return (
-    <Layout>
-      <PhotosProvider albumId={albumId}>
-        <div className='flex flex-col md:flex-row justify-center items-center gap-3 mt-4'>
-          <UploadButton albumId={album.id} />
-        </div>
-        <h1 className='text-2xl font-bold'>{album.name}</h1>
-        <p className='text-lg mb-4'>{album.description}</p>
-        <ImageGrid albumId={albumId} />
-      </PhotosProvider>
-    </Layout>
+    <>
+      <Head>
+        <title>Social Album | {album.name} </title>
+        <meta name='description' content={`${album.name} content`} />
+      </Head>
+      <Layout>
+        <PhotosProvider albumId={albumId}>
+          <div className='lg:flex lg:flex-1 lg:gap-8 lg:items-center min-h-screen'>
+            <div className='flex flex-col md:flex-row justify-center items-center gap-3 mt-4'>
+              <UploadButton albumId={album.id} />
+            </div>
+            <div className='w-full h-full'>
+              <h1 className='text-2xl lg:text-3xl font-bold my-4'>
+                {album.name}
+              </h1>
+              <p className='text-lg mb-4 py-3 font-semibold'>
+                {album.description}
+              </p>
+              <ImageGrid albumId={albumId} userAlbum={album.user_album} />
+            </div>
+          </div>
+        </PhotosProvider>
+      </Layout>
+    </>
   )
 }
 
--- pages/album/create.js
+++ pages/album/create.js
@@ -1,29 +1,36 @@
 import { Form } from 'components/Form'
 import { Layout } from 'components/Layout'
 import { IoIosArrowBack } from 'react-icons/io'
+import Head from 'next/head'
 import Link from 'next/link'
 
 export default function Create() {
   return (
-    <Layout>
-      <div className='h-full w-full'>
-        <div className='relative w-full md:w-3/4 lg:w-1/2 flex justify-center items-center mx-auto'>
-          <div className='absolute left-0 flex justify-start items-center'>
-            <Link href={'/'}>
-              <a className=' bg-zinc-700 px-2 py-1 md:px-4 md:py-2 rounded hover:bg-zinc-800 ease duration-200'>
-                <IoIosArrowBack size='1.5rem' />
-              </a>
-            </Link>
+    <>
+      <Head>
+        <title>Social Album | Create Album </title>
+        <meta name='description' content='create page for new albums' />
+      </Head>
+      <Layout>
+        <div className='h-full w-full'>
+          <div className='relative w-full md:w-3/4 lg:w-1/2 flex justify-center items-center mx-auto'>
+            <div className='absolute left-0 flex justify-start items-center'>
+              <Link href={'/'}>
+                <a className=' bg-zinc-700 px-2 py-1 md:px-4 md:py-2 rounded hover:bg-zinc-800 ease duration-200'>
+                  <IoIosArrowBack size='1.5rem' />
+                </a>
+              </Link>
+            </div>
+            <h1 className='text-xl md:text-3xl text-center font-bold my-2 w-full'>
+              Create an album
+            </h1>
           </div>
-          <h1 className='text-xl md:text-3xl text-center font-bold my-2 w-full'>
-            Create an album
-          </h1>
-        </div>
 
-        <div className='flex flex-col justify-center items-center'>
-          <Form />
+          <div className='flex flex-col justify-center items-center'>
+            <Form />
+          </div>
         </div>
-      </div>
-    </Layout>
+      </Layout>
+    </>
   )
 }
