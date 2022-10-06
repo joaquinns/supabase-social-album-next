98854c6a151b0df173e7e9d6f6b5a60e02e5c6a5
joaquinns <joaquindevv96@gmail.com>
2022-10-06 13:44:03 -0400

    added spinner props

--- components/Spinner/index.js
+++ components/Spinner/index.js
@@ -1,5 +1,13 @@
-export const Spinner = () => {
+import PropTypes from 'prop-types'
+
+export const Spinner = ({ size = 1.5 }) => {
   return (
-    <div className='border-4 border-gray-500 border-t-teal-500 border-t-4 animate-spin rounded-full h-6 w-6 flex mx-auto justify-center'></div>
+    <div
+      className={`border-4 border-gray-500 border-t-teal-500 border-t-4 animate-spin rounded-full h-[${size}rem] w-[${size}rem] flex mx-auto justify-center`}
+    ></div>
   )
+}
+
+Spinner.propTypes = {
+  size: PropTypes.number
 }
