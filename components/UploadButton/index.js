a0c234212eb6eee65e97cc872f14a23876b5596b
joaquinns <joaquindevv96@gmail.com>
2022-10-06 13:44:33 -0400

    added styles to button

--- components/UploadButton/index.js
+++ components/UploadButton/index.js
@@ -1,19 +1,24 @@
 import { useState } from 'react'
 import PropTypes from 'prop-types'
 import { UploadPhotoForm } from 'components/UploadPhotoForm'
+import { IoAdd } from 'react-icons/io5'
 
 export const UploadButton = ({ albumId }) => {
   const [showForm, setShowForm] = useState(false)
 
   return (
     <>
-      {showForm && <UploadPhotoForm albumId={albumId} />}
-      <button
-        className='bg-zinc-700 p-4 font-bold text-xl rounded'
-        onClick={() => setShowForm(!showForm)}
-      >
-        Add photo
-      </button>
+      {showForm && (
+        <UploadPhotoForm albumId={albumId} closeForm={setShowForm} />
+      )}
+      {!showForm && (
+        <button
+          className='bg-blue-600 p-3 font-bold text-xl rounded-full hover:bg-blue-800 ease duration-200'
+          onClick={() => setShowForm(!showForm)}
+        >
+          <IoAdd size='2rem' color='white' />
+        </button>
+      )}
     </>
   )
 }
