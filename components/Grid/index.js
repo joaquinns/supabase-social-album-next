145f84bdcbec53d9352357fbdce8738fcf7fa67b
joaquinns <joaquindevv96@gmail.com>
2022-10-06 13:41:57 -0400

    custom style to min-h

--- components/Grid/index.js
+++ components/Grid/index.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types'
 
 export const Grid = ({ children }) => {
   return (
-    <div className='grid grid-cols-auto-grid gap-3 lg:gap-2 justify-center items-center'>
+    <div className='grid grid-cols-auto-grid gap-3 lg:gap-2 justify-center items-center min-h-full'>
       {children}
     </div>
   )
