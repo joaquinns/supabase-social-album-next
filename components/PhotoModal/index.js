1570d95dbd70b36f3440529ffcfcc554fe857301
joaquinns <joaquindevv96@gmail.com>
2022-10-06 13:43:43 -0400

    edited styles

--- components/PhotoModal/index.js
+++ components/PhotoModal/index.js
@@ -6,10 +6,12 @@ import { useRouter } from 'next/router'
 import { usePhotosAlbum } from 'context/photosContext'
 import { AiFillDelete } from 'react-icons/ai'
 import { BsBookmarkStarFill } from 'react-icons/bs'
+import { useAuth } from 'context/authContext'
 
-export const PhotoModal = ({ photo }) => {
+export const PhotoModal = ({ photo, userAlbum }) => {
   const { setPhotos } = usePhotosAlbum()
   const { handleCloseModal, handleModal, showModal } = useModal()
+  const { user } = useAuth()
   const router = useRouter()
 
   const handleMakeCover = async (albumId, imgURL) => {
@@ -41,44 +43,45 @@ export const PhotoModal = ({ photo }) => {
   return (
     <div className='relative h-[150px] w-full'>
       <Modal isOpen={showModal} closeModal={handleCloseModal}>
-        {console.log(photo.id)}
         <article className='flex flex-1 lg:flex-none items-center h-full flex-col gap-2 lg:flex-row'>
           <div className='relative w-full lg:w-3/4 h-full'>
             <Image
               src={photo.img_url}
               layout='fill'
-              className='rounded'
+              className='rounded-t md:rounded-l'
               objectFit='cover'
               alt='photo of the album'
             />
           </div>
-          <div className='w-full lg:w-1/3 h-1/3 lg:h-full flex flex-col justify-center lg:justify-start gap-2'>
-            <p className='h-full p-2 rounded font-semibold'>
+          <div className='w-full lg:w-1/3 h-1/3 lg:h-full flex flex-col justify-center lg:justify-start gap-2 p-2'>
+            <p className='h-full p-1 rounded font-semibold overflow-y-auto'>
               {photo.description}
             </p>
 
-            <div className='flex gap-1 justify-between items-center'>
-              <button
-                onClick={() =>
-                  handleMakeCover(photo.photo_album, photo.img_url)
-                }
-                className='py-2 px-3 md:px-4 text-sm md:text-base bg-zinc-900 hover:opacity-60 rounded font-bold flex gap-2 items-center justify-center'
-              >
-                Cover
-                <span>
-                  <BsBookmarkStarFill size='1.3rem' />
-                </span>
-              </button>
-              <button
-                onClick={() => handleDeletePhoto(photo.id, photo.photo_album)}
-                className='py-2 px-3 md:px-4 text-sm md:text-base bg-zinc-700 flex gap-2 items-center justify-center hover:bg-red-600 ease duration-200 rounded font-bold'
-              >
-                Delete
-                <span>
-                  <AiFillDelete size='1.3rem' />
-                </span>
-              </button>
-            </div>
+            {user && user.id === userAlbum && (
+              <div className='flex gap-1 justify-between items-center'>
+                <button
+                  onClick={() =>
+                    handleMakeCover(photo.photo_album, photo.img_url)
+                  }
+                  className='py-2 px-3 md:px-4 text-sm md:text-base bg-zinc-900 hover:opacity-60 rounded font-bold flex gap-2 items-center justify-center'
+                >
+                  Cover
+                  <span>
+                    <BsBookmarkStarFill size='1.3rem' />
+                  </span>
+                </button>
+                <button
+                  onClick={() => handleDeletePhoto(photo.id, photo.photo_album)}
+                  className='py-2 px-3 md:px-4 text-sm md:text-base bg-zinc-700 flex gap-2 items-center justify-center hover:bg-red-600 ease duration-200 rounded font-bold'
+                >
+                  Delete
+                  <span>
+                    <AiFillDelete size='1.3rem' />
+                  </span>
+                </button>
+              </div>
+            )}
           </div>
         </article>
       </Modal>
